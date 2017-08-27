a9c357d0638962c644d98e667f5ba739ec8e3f29
zacharysang <zacharysang@gmail.com>
2017-08-26 21:05:13 -0400

    Added gitignore, predict.py, and data

---
+++ predict.py
@@ -0,0 +1,5 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import GBTClassifier
+from pyspark.ml.feature import StringIndexer, VectorIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
