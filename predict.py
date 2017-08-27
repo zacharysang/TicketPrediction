52eb5f67f1d517f79967ee22b739439247754b92
zacharysang <zacharysang@gmail.com>
2017-08-26 21:35:11 -0400

    Added .venv to .gitignore. Added comments to predict.py

--- predict.py
+++ predict.py
@@ -3,3 +3,15 @@ from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
+PATH = './Traffic_Stops__drivers.csv'
+
+#load the data
+data = spark.read.format('csv').load(PATH)
+
+# Convert Interview date to interview time
+
+# Action taken column -> Citation | No Citation
+
+# Address -> Road
+
+# Address (Road) | Action Taken Id | Date (Time) | Vehicle Make | Vehicle Model | Vehicle Year | License Plate State | Sex | Race | Age range
